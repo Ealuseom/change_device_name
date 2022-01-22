83ae9cc375fed5caaaed433a0bdefde5034a3817
Ealuseom <Lukas.Vieweg@outlook.de>
2022-01-22 03:39:48 +0100

    fml, done for today

--- change_device_name.ps1
+++ change_device_name.ps1
@@ -1,9 +1,14 @@
 $name = New-Object PSObject -Property @{
     type          = "NB"
     Room          = 211
-    counterstring = Get-Content C:\Users\Admin\Desktop\counter.txt
-    counterint    = [int]$name.counterstring
+    counterstring = Get-Content C:\_repos\Arbeit\Change_Device_Name\counter.txt
+    #   counterint    = [int]$name.counterstring
 }
-Write-Host $name.type - $name.room - $name.counterstring
-$name.counterint = $name.counterint + 01
-Set-Content C:\Users\Admin\Desktop\counter.txt $name.counterint
+Write-Host $name.counterstring
+#$name.counterint = $name.counterint + 1
+#Set-Content C:\Users\Admin\Desktop\counter.txt $name.counterint
+if ($name.counterstring -lt 10) {
+    $sugma = "", $name.counterstring
+    $sugma -join "0"
+    Set-Content C:\_repos\Arbeit\Change_Device_Name\counter.txt $sugma -join "0"
+}
