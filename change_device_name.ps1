6aa86959b2264b1f03d1433cd58c21271eb27e2b
Ealuseom <Lukas.Vieweg@outlook.de>
2022-01-22 17:27:37 +0100

    Removed useless code

--- change_device_name.ps1
+++ change_device_name.ps1
@@ -4,15 +4,8 @@ $counter = Get-Content C:\_repos\Arbeit\Change_Device_Name\counter.txt
 
 $i = [System.Decimal]::Parse($counter)
 $i++
-if ($i -lt 10) {
-    $formatted = '{0:d2}' -f [int]$i
-    Set-Content C:\_repos\Arbeit\Change_Device_Name\counter.txt $formatted
-    $device_name = "$type-$name-$formatted"
-}
-else {
-    Set-Content C:\_repos\Arbeit\Change_Device_Name\counter.txt $i
-    Write-Host $i
-    $device_name = "$type-$name-$i"
-}
+$formatted = '{0:d2}' -f [int]$i
+Set-Content C:\_repos\Arbeit\Change_Device_Name\counter.txt $formatted
+$device_name = "$type-$name-$formatted"
 Write-Host $device_name
-Rename-Computer -NewName $device_name -Force
+#Rename-Computer -NewName $device_name -Force
